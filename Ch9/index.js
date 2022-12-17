9d5241c22ce3a3c5c3087854c1ad5c91ceb686a1
M Bilal <bilaldarmuhammad@gmail.com>
2022-12-17 20:11:22 +0500

    URL Modules

---
+++ Ch9/index.js
@@ -0,0 +1,14 @@
+//URL Module
+import { URL } from 'url';
+const myURL = new URL('http://www.example.com:8080/p/a/t/h?query=string#hash');
+console.log(myURL.hash);
+console.log(myURL.host);
+console.log(myURL.hostname);
+console.log(myURL.href);
+console.log(myURL.pathname);
+console.log(myURL.port);
+console.log(myURL.protocol);
+console.log(myURL.search);
+console.log(myURL.searchParams);
+console.log(myURL.toString());
+console.log(myURL.toJSON());
