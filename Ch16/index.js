1c6575f6bf68493fcac1278c6249a57c1ff5570b
M Bilal <bilaldarmuhammad@gmail.com>
2022-12-21 09:46:14 +0500

    import and export ES6

---
+++ Ch16/index.js
@@ -0,0 +1,8 @@
+//Import and export modules ES6
+import fullname from "./student.js";
+console.log(fullname)
+
+
+// import { name, marks } from "./student.js";
+// console.log(name);
+// marks(10, 20);
---
+++ Ch16/student.js
@@ -0,0 +1,12 @@
+//Import and export modules ES6
+const name = "Bilal Dar";
+export default name;
+
+
+
+// const marks = (math, sci) => {
+//     console.log("Total marks is: ", math + sci)
+// }
+// export { name, marks };
+
+
